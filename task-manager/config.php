e9d3bb8eb94c180ec23307202abfd7636628b84c
Sofisazz <gmariya6666@gmail.com>
2025-09-20 12:36:08 +0300

    add config

---
+++ task-manager/config.php
@@ -0,0 +1,14 @@
+<?php
+// config.php
+$host = 'localhost';
+$dbname = 'task_manager';
+$username = 'root';
+$password = 'mysql';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Ошибка подключения к базе данных: " . $e->getMessage());
+}
+?>
